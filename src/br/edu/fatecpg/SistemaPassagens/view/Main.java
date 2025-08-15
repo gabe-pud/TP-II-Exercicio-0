b89e991dfc15866ea6db0a8a44fc87d9e8d29c5a
gabe-pud <gabrielgarciasantana3@gmail.com>
2025-08-15 00:27:36 -0300

    test: object instantiation & method execution

--- src/br/edu/fatecpg/SistemaPassagens/view/Main.java
+++ src/br/edu/fatecpg/SistemaPassagens/view/Main.java
@@ -1,8 +1,28 @@
 package br.edu.fatecpg.SistemaPassagens.view;
 
 
+import br.edu.fatecpg.SistemaPassagens.controller.*;
+
 public class Main {
     public static void main(String[] args) {
+        AeroportoController aeroporto = new AeroportoController();
 
+        aeroporto.addVoo("123","SP","CE",25,250,true,false);
+        aeroporto.addVoo("456","US","CA",40,875,false,true);
+
+        aeroporto.exibirVoos();
+
+        System.out.println("criando passagem:\n");
+        aeroporto.criarPassagem(aeroporto.buscarVoo(1),2);
+        aeroporto.exibirPassagens();
+
+        System.out.println("quantidade de assentos disponiveis antes do pagamento:\n");
+        aeroporto.buscarVoo(1).infoVoo();
+
+        System.out.println("-------");
+        aeroporto.buscarPassagem(0).realizarPagamento();
+
+        System.out.println("quantidade de assentos disponiveis depois do pagamento:\n");
+        aeroporto.buscarVoo(1).infoVoo();
     }
 }
